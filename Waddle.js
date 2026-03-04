bce507fefb0cc7012e6551e4c435a3d87a9facb8
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-03-04 18:26:41 +0000

    Update Waddle.js

--- Waddle.js
+++ Waddle.js
@@ -815,7 +815,7 @@ function startTargetHUDLoop() {
     let h = now.getHours();
     const m = String(now.getMinutes()).padStart(2, '0');
     const s = String(now.getSeconds()).padStart(2, '0');
-    const ampm = h >= 12 ? 'PM' : 'AM';
+    const ampm = h > 12 ? 'PM' : 'AM';
     h = h % 12 || 12;
     updateCounterText('realTime', `${h}:${m}:${s} ${ampm}`);
   }
