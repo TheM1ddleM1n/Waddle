ad320378dcff5362ce773ab1f95ff90826dec7ad
Duckyy <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-03-29 14:22:08 +0100

    Update Waddle.user.js
    yay

--- Waddle.user.js
+++ Waddle.user.js
@@ -42,19 +42,11 @@ document.title = `🐧 Waddle v${SCRIPT_VERSION}`;
     return e;
   };
   const div = (cls, text) => el('div', cls, text);
-  function roundRect(ctx, x, y, w, h, r) {
-    ctx.beginPath();
-    ctx.moveTo(x + r, y);
-    ctx.lineTo(x + w - r, y);
-    ctx.quadraticCurveTo(x + w, y, x + w, y + r);
-    ctx.lineTo(x + w, y + h - r);
-    ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
-    ctx.lineTo(x + r, y + h);
-    ctx.quadraticCurveTo(x, y + h, x, y + h - r);
-    ctx.lineTo(x, y + r);
-    ctx.quadraticCurveTo(x, y, x + r, y);
-    ctx.closePath();
-  }
+  const roundRect = (ctx, x, y, w, h, r) => {
+  ctx.beginPath();
+  ctx.roundRect(x, y, w, h, r);
+  ctx.closePath();
+};
 
   function getPlayerUsername() {
     try {
