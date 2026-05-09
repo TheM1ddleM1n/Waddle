c7407f7c349217bd5c3ad932cf53363f7bc8348d
TheM1ddleM1n <bateb022@gmail.com>
2026-05-09 18:42:11 +0100

    Update Waddle.user.js

--- Waddle.user.js
+++ Waddle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Waddle
 // @namespace    https://github.com/TheM1ddleM1n/Waddle
-// @version      9.1
+// @version      9.4
 // @description  The ULTIMATE miniblox.io enhancement suite! Have fun!
 // @author       Scripter, TheM1ddleM1n, heythereu
 // @icon         https://raw.githubusercontent.com/TheM1ddleM1n/Waddle/refs/heads/main/Penguin.png
@@ -9,7 +9,7 @@
 // @run-at       document-start
 // ==/UserScript==
 
-const SCRIPT_VERSION = '9.1';
+const SCRIPT_VERSION = '9.4';
 
 (function () {
   'use strict';
